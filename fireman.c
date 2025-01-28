10204f53a6b932b290e6bf2b6156b15739c3e664
dromeczek <161317570+dromeczek@users.noreply.github.com>
2025-01-28 02:52:00 +0100

    Update fireman.c z komentarzami

--- fireman.c
+++ fireman.c
@@ -33,29 +33,35 @@ void sem_p(int semid, int semnum) { sem_op(semid, semnum, -1); }
 void sem_v(int semid, int semnum) { sem_op(semid, semnum, 1); }
 
 int main() {
+// Uzyskanie pamięci współdzielonej za pomocą klucza SHM_KEY
     int shmid = shmget(SHM_KEY, sizeof(struct shared_data), 0666);
     if (shmid == -1) {
         perror("Nie można uzyskać pamięci współdzielonej");
         exit(1);
     }
-
+// Podłączenie pamięci współdzielonej
     struct shared_data *data = shmat(shmid, NULL, 0);
     if (data == (void *)-1) {
         perror("Nie można podłączyć pamięci współdzielonej");
         exit(1);
     }
-
+// Uzyskanie semafora za pomocą klucza SEM_KEY
     int semid = semget(SEM_KEY, 1, 0666);
     if (semid == -1) {
         perror("Nie można uzyskać semafora");
         exit(1);
     }
-
+ // Zablokowanie dostępu do pamięci współdzielonej (operacja P)
     sem_p(semid, 0);
+// Zgłoszenie alarmu pożarowego
     printf("\033[0;37;41mStrażak: Pożar zgłoszony!\033[0m\n");
+// Zakończenie działania generatora klientów
     system("pkill  generator");
+// Zakończenie działania klientów
     system("pkill  client");
+// Ustawienie flagi alarmu pożarowego w pamięci współdzielonej
     data->alarm_pozarowy = 1;
+ // Odblokowanie dostępu do pamięci współdzielonej (operacja V)
     sem_v(semid, 0);
 
     return 0;
