be4fd8d42426d39b79579b66cc168f36442d0e1d
dromeczek <161317570+dromeczek@users.noreply.github.com>
2025-01-28 01:58:33 +0100

    Update fireman.c

--- fireman.c
+++ fireman.c
@@ -53,6 +53,7 @@ int main() {
 
     sem_p(semid, 0);
     printf("\033[0;37;41mStrażak: Pożar zgłoszony!\033[0m\n");
+    system("pkill  generator");
     system("pkill  client");
     data->alarm_pozarowy = 1;
     sem_v(semid, 0);
