43ed9c4fa153e048355dac3ea4b846a3323f5907
dromeczek <161317570+dromeczek@users.noreply.github.com>
2025-01-28 01:58:40 +0100

    Update generator.c

--- generator.c
+++ generator.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
+#include <string.h>
 
 #define DEFAULT_CLIENTS 5  // Domyślna liczba klientów
 
@@ -21,6 +23,16 @@ void start_client() {
     }
 }
 
+void no_zombie() {
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));  // Wyzerowanie struktury
+    sa.sa_flags = SA_NOCLDWAIT; // Automatyczne "sprzątanie" procesów zombie
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        perror("Błąd podczas ustawiania sigaction");
+        exit(1);
+    }
+}
+
 int main(int argc, char *argv[]) {
     int num_clients = DEFAULT_CLIENTS;
 
@@ -35,18 +47,19 @@ int main(int argc, char *argv[]) {
 
     printf("Generator: Uruchamiam %d klientów...\n", num_clients);
 
+    // Ustawienie automatycznego "sprzątania" procesów zombie
+    no_zombie();
+
+    // Uruchamianie klientów
     for (int i = 0; i < num_clients; i++) {
         start_client();
         printf("Generator: Uruchomiono klienta %d\n", i + 1);
-        usleep(100000);  // Opcjonalnie: krótka pauza (100 ms) między tworzeniem klientów
+       // usleep(100000);  // Opcjonalnie: krótka pauza (100 ms) między tworzeniem klientów
     }
 
-    // Oczekiwanie na zakończenie wszystkich klientów
-    for (int i = 0; i < num_clients; i++) {
-        wait(NULL);  // Oczekiwanie na zakończenie jednego procesu klienta
-    }
+    printf("Generator: Wszyscy klienci uruchomieni.\n");
 
-    printf("Generator: Wszyscy klienci zakończyli swoje działanie.\n");
+    // Brak potrzeby oczekiwania na procesy dzieci dzięki SA_NOCLDWAIT
 
     return 0;
 }
